8b5a9f98cae9b87a3a770835ca0c32c1a92f6ba2
Billy Irwin <birwin93@gmail.com>
2012-10-15 13:38:07 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urlbits::Application.config.secret_token = 'f37e5f6571afebad1dce4114b4b572e202936982d0e90d3453f81ad342a5dce9cb53098a27a5288088a3bf0ba4454ae3b2a5e852be5231fcb6e5962683cf1f2b'
